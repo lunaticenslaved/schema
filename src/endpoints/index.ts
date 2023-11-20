439a4bd252aa554bdbfe45fd0623d6e206df8ab8
lunaticenslaved <lunaticenslaved@yandex.ru>
2023-11-20 23:08:15 +0300

    feat: add setEndpoint

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -6,7 +6,11 @@ function create(host: keyof typeof map, path: string) {
   return `${map[host]}/${path.replace(/^[//]/, '')}`;
 }
 
+function setEndpoint(host: keyof typeof map, path: string) {
+  map[host] = path;
+}
+
 export const Endpoint = {
-  map,
   create,
+  setEndpoint,
 };
