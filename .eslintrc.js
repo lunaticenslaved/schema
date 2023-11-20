3c5a7c12d1ceedcf4da8c67942d7e90a45e4262c
lunaticenslaved <lunaticenslaved@yandex.ru>
2023-11-20 22:36:50 +0300

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,15 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['prettier', '@typescript-eslint'],
+  plugins: ['prettier', '@typescript-eslint', 'unused-imports'],
   rules: {
     'no-console': 'warn',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
     '@typescript-eslint/no-namespace': 0,
     '@typescript-eslint/no-unused-vars': [
       'error',
