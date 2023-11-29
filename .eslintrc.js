7346c62128237ff34bc0e74d620ee8f8707470f8
lunaticenslaved <lunaticenslaved@yandex.ru>
2023-11-29 22:47:32 +0300

    ci: add custom build and publish scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   extends: ['./node_modules/@lunaticenslaved/configs/eslint.base.js'],
-  ignorePatterns: ['dist', 'node_modules'],
+  ignorePatterns: ['dist', 'node_modules', 'scripts'],
   rules: {
     'no-console': 0,
   },
---
+++ scripts/publish.js
@@ -0,0 +1,39 @@
+const fsExtra = require('fs-extra');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+if (!fsExtra.existsSync('dist')) {
+  throw new Error('Run build before publish!');
+}
+
+fsExtra.mkdirSync('dist-copy');
+fsExtra.copySync('dist', `dist-copy/dist`);
+fsExtra.removeSync('dist');
+fsExtra.renameSync('dist-copy', 'dist');
+
+// update package json version
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+const args = process.argv.slice(2);
+
+const versionIndex = args.findIndex(arg => arg === '--version');
+const version = versionIndex > -1 ? args[versionIndex + 1] : 'patch';
+
+const [major = 0, minor = 0, patch = 0] = packageJson.version.split('.').map(Number);
+
+if (version === 'patch') {
+  packageJson.version = `${major}.${minor}.${patch + 1}`;
+} else if (version === 'minor') {
+  packageJson.version = `${major}.${minor + 1}.${0}`;
+} else {
+  packageJson.version = `${major + 1}.${0}.${0}`;
+}
+
+fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+
+const paths = ['.npmignore', 'package.json', 'package-lock.json'];
+
+for (const path of paths) {
+  fsExtra.copySync(path, `dist/${path}`);
+}
+
+execSync('cd dist && npm publish --dry-run');
